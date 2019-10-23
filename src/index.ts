80d686a0e7ebae703996d85f5bd238d4e823af93
Murari <murari.nayak@bridgera.com>
2019-10-23 11:49:54 +0530

    Build check 20191023 4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import { ChatServer } from './chat-server';
+// import express from 'express';
+// const app = express();
 
-const app = new ChatServer().getApp();
-export { app };
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
