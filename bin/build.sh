9674b5254c024cc3e6b216e7ee1e1fd08dbd381b
Murari <murari.nayak@bridgera.com>
2019-10-23 16:58:16 +0530

    Build check 20191023 26

--- bin/build.sh
+++ bin/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo 'Delete the dist folder';
+echo '==========> delete the dist folder'
 rm -rf ./dist
-echo 'install and build server code';
+echo '==========> install and build server code'
 npm install --prefix server
-echo 'move files to top level';
+echo '==========> move files to top level'
 mv ./server/dist ./dist
-echo 'install client related module';
+echo '==========> install client related module'
 npm install --prefix client
-echo 'Build client code';
+echo '==========> build client code'
 npm run build:prod --prefix client
-echo 'move files to top level';
+echo '==========> move files to top level'
 mv ./client/dist ./dist/public
+echo '==========> build complete'
